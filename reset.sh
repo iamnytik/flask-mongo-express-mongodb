55e0f863a23b97dc0dc46384088b53378d10ed17
Nytik Birudavolu <91942626+iamnytik@users.noreply.github.com>
2023-04-20 08:40:07 +0530

    updated reset.sh

--- reset.sh
+++ reset.sh
@@ -1,11 +1,12 @@
 minikube stop
 minikube delete
-
 minikube start 
-source ~/.bashrc
+#source ~/.bashrc
+eval $(minikube -p minikube docker-env) #linux
+minikube docker-env | Invoke-Expression  #powershell 
 kubectl apply -f configmap.yaml
 kubectl apply -f secret.yaml
 kubectl apply -f secret.yaml
-kubectl apply -f deployments.yaml
 docker build -t flask-app-image -f  flask-app-image.dockerfile .
+kubectl apply -f deployments.yaml
 
